2e99b60d9a3c4a19a56c6e078a2dfaefbd2869d2
Eena Salorinta <eenasalorinta@Eena-MacBook-Pro.local>
2019-09-17 19:10:39 +0300

    ex04

--- piscine/d02/ex04/ft_print_comb.c
+++ piscine/d02/ex04/ft_print_comb.c
@@ -7,7 +7,35 @@ void	ft_putchar(char c)
 
 void	ft_print_comb(void)
 {
-  
+  int x;
+  int y;
+  int a;
+
+  x = 0;
+  y = 1;
+  a = 2;
+  while (x <= 7)
+    {
+      while (y < 9)
+	{
+	  while (a < 10)
+	    {
+	      ft_putchar(x + '0');
+	      ft_putchar(y + '0');
+	      ft_putchar(a + '0');
+	      if (x == 7 && y == 8 && a == 9)
+		return ;
+	      ft_putchar(',');
+	      ft_putchar(' ');
+	      a++;
+	    }
+	  y++;
+	  a = y + 1;
+	}
+      x++;
+      y = x + 1;
+      a = y + 1;
+    }
 }
 
 int	main(void)
